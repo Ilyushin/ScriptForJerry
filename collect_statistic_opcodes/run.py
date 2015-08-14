dfff54b50c6124bea84ff753f84a34f898591171
Ilyushin <jon.ilyushin@gmail.com>
2015-08-14 15:31:38 +0300

    Add jerry_opcodes and result files

---
+++ collect_statistic_opcodes/run.py
@@ -0,0 +1,25 @@
+import sys, subprocess, os
+
+if len(sys.argv) == 1:
+    #set parameters
+    #path_tests = os.path.abspath(sys.argv[1])
+    dir = os.path.dirname(__file__)
+    path_jerry = path_jerry = os.path.join(dir, 'jerry_opcodes')
+    path_result_folder = os.path.join(dir, 'results')
+    path_tests = "/home/john/git/ScriptForJerry/test_closure_jerry/test_files"
+    
+    tests = []
+    for fn in os.listdir(path_tests):
+       if fn.lower().endswith('.js'):
+           tests.append(path_tests+'/'+fn)
+           
+    if tests:
+        for test in tests:
+            file_name = os.path.basename(test)
+            cmd = path_jerry + ' ' + test+' >'+path_result_folder+'/'+file_name.split('.')[0]+'.csv'
+            print 'start - '+test
+            r = subprocess.call(cmd, shell=True)
+            print 'end - '+test
+         
+    print 'success'
+            
